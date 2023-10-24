5e9891600da214962db6038092f737f79e628ef6
matteo-fava <matteo.luca.fava@gmail.com>
2023-10-24 13:39:15 +0200

    es8 parziale

--- lab4/es8.cpp
+++ lab4/es8.cpp
@@ -1,6 +1,27 @@
 //24.10.23 by Matteo Fava
 
+#include <iostream>
+using namespace std;
+
+//elimina il valore in indice where da un vettore
+//posizionando nell'ultima cella
+int delete_entry_swap(float[], int, int, int);
+
 int main(){
 
     return 0;
+}
+
+int delete_entry_swap(float v[], int size, int used, int where){
+    if (where>= 0 && where<=used-2){
+        for (int i = where; i < used-1; i++ ){
+            std::swap(v[i], v[i+1]);
+        }
+
+        return used-1;
+
+    } else {
+        cout << "Eliminazione non possibile: indice fuori range" << endl;
+        return used;
+    }
 }
