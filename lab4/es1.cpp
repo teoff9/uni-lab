eb113f522fa10eb6a6de36e9b93864fc21bfaa9a
matteo-fava <matteo.luca.fava@gmail.com>
2023-10-24 11:58:03 +0200

    es1 e es2

--- lab4/es1.cpp
+++ lab4/es1.cpp
@@ -1,8 +1,24 @@
 //24.10.23 by Matteo Fava
 
+#include <iostream>
+using namespace std;
+
+#define N 10
+
 int main(){
-    int data[10];
-    
+    int data[N];
+    int size = N;
+    int used = 0;
+
+    for (used; used < 10; used++){
+        cin >> data[used];
+    }
+
+    cout << "printing elements of data:" << endl;
+
+    for (int a : data){
+        cout << a << endl;
+    }
 
     return 0;
 }
--- lab4/es2.cpp
+++ lab4/es2.cpp
@@ -1,6 +1,33 @@
 //24.10.23 by Matteo Fava
 
-int main(){
+#include <iostream>
+using namespace std;
 
+#define N 10
+
+int main(){
+    int data[N];
+    int size = N;
+    int used = 0;
+    int tmp;
+
+    while(used < size){
+        cin >> tmp;
+        if (tmp == 0){
+            break;
+        } else {
+            data[used] = tmp;
+            used++;
+        }
+    }
+
+    if (used != 0) {
+        cout << "printing elements of data: " << used << endl;
+
+    for (int i = 0; i < used ; i++){
+        cout << data[i] << endl;
+    }
+    }
+    
     return 0;
 }
