c1210429a7527b5bdb524b0e42ec3bc363d2cad5
matteo-fava <matteo.luca.fava@gmail.com>
2023-10-31 12:50:01 +0100

    finished es4

--- lab5/es4.cpp
+++ lab5/es4.cpp
@@ -1,6 +1,28 @@
 //31.10.2023 by Matteo Fava
 
+#include <fstream>
+#include <iostream>
+using namespace std;
+
 int main(){
+    ifstream data;
+    ofstream output;
+    float tmp;
+
+    data.open("dati/data.dat");
+    if (data.fail()){
+        cout << "Failed to read file." << endl;
+        return -1;
+    }
+
+    output.open("dati/output.dat");
+
+    data >> tmp;
+    while (!data.eof()){
+        output << tmp << "\n";
+        data >> tmp;
+    }
+    output.close();
 
     return 0;
 }
