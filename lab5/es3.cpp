0a8b18fb00685488229d1e2d3e256338a724e7fb
matteo-fava <matteo.luca.fava@gmail.com>
2023-10-31 12:38:38 +0100

    added es3

--- lab5/es3.cpp
+++ lab5/es3.cpp
@@ -1,6 +1,30 @@
 //31.10.2023 by Matteo Fava
 
+#include <fstream>
+#include <iostream>
+using namespace std;
+
 int main(){
+    
+    ifstream data;
+    int c = 0;
+    float tmp;
+
+    data.open("dati/data.dat");
+    if (data.fail()){
+        cout << "Failed to read file." << endl;
+        return -1;
+    }
+
+    data >> tmp;
+    while (!data.eof()){
+        c++;
+        data >> tmp;
+    }
+    
+    data.close();
+
+    cout << c << endl;
 
     return 0;
 }
