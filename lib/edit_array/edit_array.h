bbdb31add101121dda75f615a3a556522aab54b9
matteo-fava <matteo.luca.fava@gmail.com>
2023-11-07 12:09:35 +0100

    created edit_array

---
+++ lib/edit_array/edit_array.h
@@ -0,0 +1,12 @@
+//07.11.2023 by Matteo Fava
+
+//Funzione che elimina l'elemento di pos where svolgendo uno shift a sinistra
+//v = array, size = n di elementi di v, used = n di elementi usati di v, where = pos di elem da eliminare
+//Return il nuovo used
+int delete_entry_shift(float v[], int size, int used, int where);
+
+
+//Funzione che elimina l'elemento di pos where
+//v = array, size = n di elementi di v, used = n di elementi usati di v, where = pos di elem da eliminare
+//Return il nuovo used
+int delete_entry_swap(float v[], int size, int used, int where);
