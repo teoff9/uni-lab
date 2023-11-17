c74f7ccec23fbb0c7acdb05268150d4e6f0361a2
matteo-fava <matteo.luca.fava@gmail.com>
2023-11-17 18:42:12 +0100

    es3.cpp

--- lab7/es3.cpp
+++ lab7/es3.cpp
@@ -3,9 +3,15 @@
 #include <iostream>
 using namespace std;
 #include <fstream>
+#include <cmath>
+#include "../lib/sorting/sorting.h"
+#include "../lib/stats/stats.h"
 
 //determina diametro medio e dev standard dei 3 colori
-void calcola_media_dev_std  (float vdiams[], char vcols[], int used, float medie[], float dev_std[]);
+void calcola_media_dev_std  (float vdiams[], char vcols[], int used, float medie[], float dev_std[], int* n);
+
+//calcola stats
+void calcolaStats(float data[], int used, float* median, float* med, float* stddev);
 
 int main(){
     ifstream data;
@@ -16,6 +22,9 @@ int main(){
     char c;
     float medie[3]; //{r,g,b}
     float dev_std[3]; //{r,g,b}
+    int n_rosse;
+    float * rdiams = NULL;
+    float median, med, stddev;
     
     //apri data
     data.open("data/sfere.dat");
@@ -40,17 +49,45 @@ int main(){
     }
 
     //determina diametro medio e dev standard delle due
+    calcola_media_dev_std(vdiams, vcols, used, medie, dev_std, &n_rosse);
+    for (int i = 0; i < 3; i++){
+        cout << medie[i] << " " << dev_std[i] << endl;
+    }
 
-
-
+    //inizializza rdiams
+    cout << "Numero di palline rosse: " << n_rosse << endl;
+    rdiams = new float[n_rosse];
+    int i =0, j = 0;
+    while (i<used && j < n_rosse){
+        if (vcols[i] == 'r'){
+            rdiams[j] = vdiams[i];
+            j++;
+        }
+        i++;
+    }
 
     //elimina arrays
+    delete []vdiams; delete []vcols;
+    vdiams = NULL; vcols = NULL;
+    
+    //sortalo
+    selection_sort(rdiams, n_rosse);
+    cout << "Mimino: " << rdiams[0] << endl;
+    cout << "Massimo: " << rdiams[n_rosse -1] << endl;
+    calcolaStats(rdiams, n_rosse, &median, &med, &stddev);
+    cout << "Mediana: " << median << endl;
+    cout << "Media: " << med << endl;
+    cout << "Deviazione Standard: " << stddev << endl;
+
+    //clean arrays
+    delete []rdiams;
+    rdiams = NULL;
 
     return 0;
 }
 
-void calcola_media_dev_std  (float vdiams[], char vcols[], int used, float medie[], float dev_std[]){
-    float sum_r[2] = {0,0}, sum_g[2] = {0,0}, sum_b[2] = {0,0};
+void calcola_media_dev_std  (float vdiams[], char vcols[], int used, float medie[], float dev_std[], int* n){
+    float sum_r[2] = {0.f,0.f}, sum_g[2] = {0.f,0.f}, sum_b[2] = {0.f,0.f};
     for (int i = 0; i<used; i++){
         if (vcols[i] == 'r'){
             sum_r[0] += vdiams[i];
@@ -69,6 +106,25 @@ void calcola_media_dev_std  (float vdiams[], char vcols[], int used, float medie
     medie[2] = sum_b[0] / sum_b[1];
 
     //calcola le dev standard
+    sum_r[0] = 0; sum_g[0] = 0; sum_b[0] = 0;
+    for (int i = 0; i<used; i++){
+        if (vcols[i] == 'r'){
+            sum_r[0] += powf(vdiams[i]-medie[0], 2.f);
+        } else if (vcols[i] == 'g'){
+            sum_g[0] += powf(vdiams[i]-medie[1], 2.f);
+        } else {
+            sum_b[0] += powf(vdiams[i]-medie[2], 2.f);
+        }
+    }
+    dev_std[0] = powf(sum_r[0] / sum_r[1], 0.5);
+    dev_std[1] = powf(sum_g[0] / sum_g[1], 0.5);
+    dev_std[2] = powf(sum_b[0] / sum_b[1], 0.5);
 
+    *n = sum_r[1];
+}
 
+void calcolaStats(float data[], int used, float* median, float* med, float* stddev){
+    *median = mediana(data, used);
+    *med = media_float(data, used);
+    *stddev = devstd_float(data, used);
 }
