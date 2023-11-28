87766264998d848d3c611ead0aace36b2f38bfec
matteo-fava <matteo.luca.fava@gmail.com>
2023-11-28 12:19:25 +0100

    pointers->reference

--- lib/spoletini/spoletini.cpp
+++ lib/spoletini/spoletini.cpp
@@ -8,28 +8,29 @@
 using namespace std;
 
 
-float* spoletini_file_float(char file_name[], int* n_dati){
+float* spoletini_file_float(char file_name[], int& n_dati){
     float* data = NULL;
     ifstream inp;
     float tmp;
-    *n_dati = 0;
+    n_dati = 0;
 
     inp.open(file_name);
     if (inp.fail()){
         cout << "Failed to load file." << endl;
+        n_dati = -1;
         return NULL;
     }
 
     //conta il numero di valori
     while (inp >> tmp){
-        (*n_dati) ++;
+        n_dati ++;
     }
     inp.close();
 
     //carica i dati
     inp.open(file_name);
-    data = new float[*n_dati];
-    for (int i = 0; i<*n_dati; i++){
+    data = new float[n_dati];
+    for (int i = 0; i<n_dati; i++){
         inp >> data[i];
     }
     inp.close();
@@ -37,28 +38,29 @@ float* spoletini_file_float(char file_name[], int* n_dati){
     return data;
 }
 
-int* spoletini_file_int(char file_name[], int* n_dati){
+int* spoletini_file_int(char file_name[], int& n_dati){
     int* data = NULL;
     ifstream inp;
     int tmp;
-    *n_dati = 0;
+    n_dati = 0;
 
     inp.open(file_name);
     if (inp.fail()){
         cout << "Failed to load file." << endl;
+        n_dati = -1;
         return NULL;
     }
 
     //conta il numero di valori
     while (inp >> tmp){
-        (*n_dati) ++;
+        n_dati ++;
     }
     inp.close();
 
     //carica i dati
     inp.open(file_name);
-    data = new int[*n_dati];
-    for (int i = 0; i<*n_dati; i++){
+    data = new int[n_dati];
+    for (int i = 0; i<n_dati; i++){
         inp >> data[i];
     }
     inp.close();
--- lib/spoletini/spoletini.h
+++ lib/spoletini/spoletini.h
@@ -6,11 +6,11 @@
 //char file_name[] : file name con path relativo o assoluto
 //char* n_dati : dato che sara' passato per side effect
 // RETURNS NULL IF SOMETHING WENT WRONG
-float* spoletini_file_float(char file_name[], int* n_dat);
+float* spoletini_file_float(char file_name[], int& n_dat);
 
 //funzione che restituisce un vettore di int di n_dat allocato dinamicamente
 //char file_name[] : file name con path relativo o assoluto
 //char* n_dati : dato che sara' passato per side effect
 // RETURNS NULL IF SOMETHING WENT WRONG
-int* spoletini_file_int(char file_name[], int* n_dat);
+int* spoletini_file_int(char file_name[], int& n_dat);
 
