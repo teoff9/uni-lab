21e3388f0c54b9fcba09adc02fa491f1b3cc860f
matteo-fava <matteo.luca.fava@gmail.com>
2023-12-28 21:36:20 +0100

    completed binary search

--- lib/search/search.cpp
+++ lib/search/search.cpp
@@ -15,3 +15,35 @@ int binary_search_int(int data[], int i, int f, int key){
 
     return j;
 }
+
+
+int binary_search_float(float data[], int i, int f, int key){
+    int j = (i+f)/2;
+    if (data[j] == key) {
+        return j;
+    } else if (i >= f) {
+        return -1;
+    } else if (data[j] > key) {
+        return binary_search_float(data, i, j-1, key);
+    } else if (data[j] < key) {
+        return binary_search_float(data, j+1, f, key);
+    }
+
+    return j;
+}
+
+
+int binary_search_double(double data[], int i, int f, int key){
+    int j = (i+f)/2;
+    if (data[j] == key) {
+        return j;
+    } else if (i >= f) {
+        return -1;
+    } else if (data[j] > key) {
+        return binary_search_double(data, i, j-1, key);
+    } else if (data[j] < key) {
+        return binary_search_double(data, j+1, f, key);
+    }
+
+    return j;
+}
--- lib/search/search.h
+++ lib/search/search.h
@@ -2,8 +2,24 @@
 //Dichiarazioni degli algoritmi di ricerca
 
 //Ricerca di un valore chiave in un array
-//ordinato crescentemente di int
+//ordinato crescentemente di double
 // int data[] : array
 //int i e f : inizio e fine
 //int key : chiave da cercare
 int binary_search_int(int data[], int i, int f, int key);
+
+
+//Ricerca di un valore chiave in un array
+//ordinato crescentemente di float
+// float data[] : array
+//int i e f : inizio e fine
+//int key : chiave da cercare
+int binary_search_float(float data[], int i, int f, int key);
+
+
+//Ricerca di un valore chiave in un array
+//ordinato crescentemente di double
+// double data[] : array
+//int i e f : inizio e fine
+//int key : chiave da cercare
+int binary_search_double(double data[], int i, int f, int key);
