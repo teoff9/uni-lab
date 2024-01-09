239799ca20dd4f12fa934cfee5c1c54a92282c5b
matteo.fava <matteo.luca.fava@gmail.com>
2024-01-09 13:11:35 +0100

    removed swap

--- lib/edit_array/edit_array.cpp
+++ lib/edit_array/edit_array.cpp
@@ -6,66 +6,78 @@ using namespace std;
 #include "edit_array.h"
 
 
-void delete_entry_shift_float(float v[], int* used, int where){
-    if (where>= 0 && where<=(*used)-1){
-        for (int i = where; i < (*used)-1; i++ ){
-            std::swap(v[i], v[i+1]);
+void delete_entry_shift_float(float v[], int &used, int where){
+    if (where>= 0 && where<=used-1){
+        for (int i = where; i < used-1; i++ ){
+            float tmp = v[i];
+            v[i] = v[i+1];
+            v[i+1] = v[i];
         }
-        (*used) -= 1;
+        used -= 1;
 
     } else {
         cout << "Eliminazione non possibile: indice fuori range" << endl;
     }
 }
 
-void delete_entry_swap_float(float v[], int* used, int where){
-    if (where>= 0 && where<=(*used)-1){
-        std::swap(v[where], v[(*used)-1]);
-        (*used) -= 1;
-
-    } else {
-        cout << "Eliminazione non possibile: indice fuori range" << endl;
-    }
-}
-
-void delete_entry_swap_bool(bool v[], int* used, int where){
-    if (where>= 0 && where<=(*used)-1){
-        std::swap(v[where], v[(*used)-1]);
-        (*used) -= 1;
-
-    } else {
-        cout << "Eliminazione non possibile: indice fuori range" << endl;
-    }
-}
-
-void delete_entry_swap_int(int v[], int* used, int where){
-    if (where>= 0 && where<=(*used)-1){
-        std::swap(v[where], v[(*used)-1]);
-        (*used) -= 1;
-
-    } else {
-        cout << "Eliminazione non possibile: indice fuori range" << endl;
-    }
-}
-
-void delete_entry_shift_bool(bool v[], int* used, int where){
-    if (where>= 0 && where<=(*used)-1){
-        for (int i = where; i < (*used)-1; i++ ){
-            std::swap(v[i], v[i+1]);
+void delete_entry_shift_bool(bool v[], int &used, int where){
+    if (where>= 0 && where<=used-1){
+        for (int i = where; i < used-1; i++ ){
+            bool tmp = v[i];
+            v[i] = v[i+1];
+            v[i+1] = v[i];
         }
-        (*used) -= 1;
+        used -= 1;
 
     } else {
         cout << "Eliminazione non possibile: indice fuori range" << endl;
     }
 }
 
-void delete_entry_shift_int(int v[], int* used, int where){
-     if (where>= 0 && where<=(*used)-1){
-        for (int i = where; i < (*used)-1; i++ ){
-            std::swap(v[i], v[i+1]);
+void delete_entry_shift_int(int v[], int &used, int where){
+     if (where>= 0 && where<=used-1){
+        for (int i = where; i < used-1; i++ ){
+            int tmp = v[i];
+            v[i] = v[i+1];
+            v[i+1] = v[i];
         }
-        (*used) -= 1;
+        used -= 1;
+
+    } else {
+        cout << "Eliminazione non possibile: indice fuori range" << endl;
+    }
+}
+
+void delete_entry_swap_float(float v[], int &used, int where){
+    if (where>= 0 && where<=used-1){
+            float tmp = v[where];
+            v[where] = v[where+1];
+            v[where+1] = v[where];
+        used -= 1;
+
+    } else {
+        cout << "Eliminazione non possibile: indice fuori range" << endl;
+    }
+}
+
+void delete_entry_swap_bool(bool v[], int &used, int where){
+    if (where>= 0 && where<=used-1){
+        bool tmp = v[where];
+        v[where] = v[where+1];
+        v[where+1] = v[where];
+        used -= 1;
+
+    } else {
+        cout << "Eliminazione non possibile: indice fuori range" << endl;
+    }
+}
+
+void delete_entry_swap_int(int v[], int &used, int where){
+    if (where>= 0 && where<=used-1){
+        int tmp = v[where];
+        v[where] = v[where+1];
+        v[where+1] = v[where];
+        used -= 1;
 
     } else {
         cout << "Eliminazione non possibile: indice fuori range" << endl;
--- lib/edit_array/edit_array.h
+++ lib/edit_array/edit_array.h
@@ -3,29 +3,29 @@
 //Funzione che elimina l'elemento di pos where
 //v = array, used = n di elementi usati di v, where = pos di elem da eliminare
 //Return il nuovo used per side effect
-void delete_entry_swap_float(float v[], int* used, int where);
+void delete_entry_swap_float(float v[], int &used, int where);
 
 //Funzione che elimina l'elemento di pos where
 //v = array, used = n di elementi usati di v, where = pos di elem da eliminare
 //Return il nuovo used per side effect
-void delete_entry_swap_bool(bool v[], int* used, int where);
+void delete_entry_swap_bool(bool v[], int &used, int where);
 
 //Funzione che elimina l'elemento di pos where
 //v = array, used = n di elementi usati di v, where = pos di elem da eliminare
 //Return il nuovo used per side effect
-void delete_entry_swap_int(int v[], int* used, int where);
+void delete_entry_swap_int(int v[], int &used, int where);
 
 //Funzione che elimina l'elemento di pos where svolgendo uno shift a sinistra
 //v = array, size = n di elementi di v, used = n di elementi usati di v, where = pos di elem da eliminare
 //Return il nuovo used per side effect
-void delete_entry_shift_float(float v[], int* used, int where);
+void delete_entry_shift_float(float v[], int &used, int where);
 
 //Funzione che elimina l'elemento di pos where svolgendo uno shift a sinistra
 //v = array, size = n di elementi di v, used = n di elementi usati di v, where = pos di elem da eliminare
 //Return il nuovo used per side effect
-void delete_entry_shift_bool(bool v[], int* used, int where);
+void delete_entry_shift_bool(bool v[], int &used, int where);
 
 //Funzione che elimina l'elemento di pos where svolgendo uno shift a sinistra
 //v = array, size = n di elementi di v, used = n di elementi usati di v, where = pos di elem da eliminare
 //Return il nuovo used per side effect
-void delete_entry_shift_int(int v[], int* used, int where);
+void delete_entry_shift_int(int v[], int &used, int where);
